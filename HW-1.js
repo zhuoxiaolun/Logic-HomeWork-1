f65480d756fb3124e2959e251fec616d6c846288
xiaolun <zhuoxl3729@gmail.com>
2023-03-08 23:41:41 +0800

    HW-1

---
+++ HW-1.js
@@ -0,0 +1,12 @@
+function show1(){
+    console.log("hello World ")
+    alert("嗨 我是視傳系")
+}
+function show2(){
+    console.log("hello World ")
+    alert("嗨! B10932014")
+}
+function show3(){
+    console.log("hello World ")
+    alert("嗨! 卓筱倫")
+}
